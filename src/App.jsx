8a384b701a056250e493b40274ee437e016a6f3c
forersa <forersa@yahoo.com>
2024-02-23 16:18:59 +0100

    Borrado y actualizacion de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-blue-600">
-      Hello world!</h1>
+    <h1 className="text-3xl font-bold underline bg-blue-600">Hello world!
+    <div className="bg-red-400 flex justify-between">
+      Soy el Padre
+      <div className="bg-red-200 w-fit">Soy el Hijo</div>
+      <div className="bg-red-300 w-fit">Soy el Hijo</div>
+      <div className="bg-red-200  w-fit">Soy el Hijo</div>
+    </div>
+    </h1>
   )
 }
 
